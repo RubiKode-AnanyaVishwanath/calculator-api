9a0518c5b6c0d8611c3f78d46d757c4f6ebbcd89
ananyavishwanath <ananya.vishwanath@rubikode.com>
2022-02-24 22:45:38 -0500

    first commit

---
+++ Divide/index.js
@@ -0,0 +1,15 @@
+module.exports = async function (context, req) {
+    try {
+        const { num1, num2 } =  context.bindingData;
+        const result = Number(num1) / Number(num2);
+        context.res = {
+            body: `${num1} / ${num2} = ${result}`
+        };
+
+    } catch (error) {
+        context.res = {
+            status: 500,
+            body: error.message
+        };
+    }
+}
